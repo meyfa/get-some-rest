be73ae829df21a67fb18af5c21674f6401f11831
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2022-11-25 14:12:56 +0100

    fix: Allow cookies to be overwritten in memory store (#36)

--- src/stores/memory-cookie-store.ts
+++ src/stores/memory-cookie-store.ts
@@ -7,6 +7,11 @@ export function memoryCookieStore (): CookieStore {
   return {
     cookies,
     putCookie (cookie) {
+      const existing = cookies.findIndex(c => c.key === cookie.key)
+      if (existing >= 0) {
+        cookies[existing] = cookie
+        return
+      }
       cookies.push(cookie)
     }
   }
