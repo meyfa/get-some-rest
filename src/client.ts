655b9de966654461b175ba0ab426f8f3f21392e1
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2022-12-02 14:48:11 +0100

    feat: Support setting custom headers (#40)

--- src/client.ts
+++ src/client.ts
@@ -3,11 +3,11 @@ import { HttpRequest, requestFromAsync } from './request.js'
 
 export type Middleware = (request: Request, next: (modified: Request) => Promise<Response>) => PromiseLike<Response>
 
-export type RequestMethod = <ResponseType = any>(path: string) => HttpRequest<ResponseType>
-export type RequestMethodWithData = <ResponseType = any>(path: string, data?: any) => HttpRequest<ResponseType>
+export type RequestMethod = <ResponseType = any>(path: string, headers?: HeadersInit) => HttpRequest<ResponseType>
+export type RequestMethodWithData = <ResponseType = any>(path: string, data?: any, headers?: HeadersInit) => HttpRequest<ResponseType>
 
 export interface HttpClient {
-  request: <ResponseType = any>(verb: CaseInsensitiveHttpVerb, path: string, data?: any) => HttpRequest<ResponseType>
+  request: <ResponseType = any>(verb: CaseInsensitiveHttpVerb, path: string, data?: any, headers?: HeadersInit) => HttpRequest<ResponseType>
   get: RequestMethod
   post: RequestMethodWithData
   put: RequestMethodWithData
@@ -27,20 +27,20 @@ async function processMiddleware (adapter: NetworkAdapter, request: Request, mid
 export function client (baseUrl: string | URL, adapter: NetworkAdapter): HttpClient {
   const middlewareStack: Middleware[] = []
 
-  const request: HttpClient['request'] = (verb, path, data) => requestFromAsync(processMiddleware(adapter, {
+  const request: HttpClient['request'] = (verb, path, data, headers) => requestFromAsync(processMiddleware(adapter, {
     url: new URL(path, baseUrl),
     method: verb,
     body: data,
-    headers: new Headers()
+    headers: new Headers(headers)
   }, middlewareStack))
 
   return {
     request,
-    get: (...args) => request('GET', ...args),
+    get: (...args) => request('GET', args[0], undefined, args[1]),
     post: (...args) => request('POST', ...args),
     put: (...args) => request('PUT', ...args),
     patch: (...args) => request('PATCH', ...args),
-    delete: (...args) => request('DELETE', ...args),
+    delete: (...args) => request('DELETE', args[0], undefined, args[1]),
     use: (middleware) => {
       middlewareStack.push(middleware)
     }
--- test/client.test.ts
+++ test/client.test.ts
@@ -1,8 +1,201 @@
 import { client } from '../src/client.js'
 import { Request, Response } from '../src/adapters/network-adapter.js'
 import { expect } from 'chai'
+import { getGlobalDispatcher, MockAgent, setGlobalDispatcher } from 'undici'
+import { fetchNetworkAdapter } from '../src/adapters/fetch-network-adapter.js'
 
 describe('client.ts', function () {
+  const mockAgent = new MockAgent()
+  mockAgent.disableNetConnect()
+
+  const mockPool = mockAgent.get('https://test.local')
+
+  const oldGlobalDispatcher = getGlobalDispatcher()
+  before(() => setGlobalDispatcher(mockAgent))
+  after(() => setGlobalDispatcher(oldGlobalDispatcher))
+
+  describe('#request()', function () {
+    it('can send GET requests', async function () {
+      const c = client(new URL('https://test.local/baz/'), fetchNetworkAdapter())
+      mockPool.intercept({ path: '/baz/1337', method: 'GET' }).reply(200, 'res1')
+      await expect(c.request('GET', '1337').expect(200)).to.eventually.equal('res1')
+    })
+
+    it('can send GET requests with headers', async function () {
+      const c = client(new URL('https://test.local/baz/'), fetchNetworkAdapter())
+      mockPool.intercept({
+        path: '/baz/1338',
+        method: 'GET',
+        headers: (headers) => new Headers(headers).get('x-test') === 'test-get-with-headers'
+      }).reply(200, 'res2')
+      await expect(c.request('GET', '1338', undefined, {
+        'X-Test': 'test-get-with-headers'
+      }).expect(200)).to.eventually.equal('res2')
+    })
+
+    it('can send POST requests without data', async function () {
+      const c = client(new URL('https://test.local/baz/'), fetchNetworkAdapter())
+      mockPool.intercept({ path: '/baz/1339', method: 'POST' }).reply(200, 'res3')
+      await expect(c.request('POST', '1339').expect(200)).to.eventually.equal('res3')
+    })
+
+    it('can send POST requests with data', async function () {
+      const c = client(new URL('https://test.local/baz/'), fetchNetworkAdapter())
+      mockPool.intercept({
+        path: '/baz/1340',
+        method: 'POST',
+        body: '{"foo":"bar"}'
+      }).reply(200, 'res4')
+      await expect(c.request('POST', '1340', { foo: 'bar' }).expect(200)).to.eventually.equal('res4')
+    })
+
+    it('can send POST requests with data and headers', async function () {
+      const c = client(new URL('https://test.local/baz/'), fetchNetworkAdapter())
+      mockPool.intercept({
+        path: '/baz/1341',
+        method: 'POST',
+        body: '{"foo":"bar"}',
+        headers: (headers) => new Headers(headers).get('x-test') === 'test-post-with-headers'
+      }).reply(200, 'res5')
+      await expect(c.request('POST', '1341', { foo: 'bar' }, {
+        'X-Test': 'test-post-with-headers'
+      }).expect(200)).to.eventually.equal('res5')
+    })
+  })
+
+  describe('#get()', function () {
+    it('can send a request', async function () {
+      const c = client(new URL('https://test.local/baz/'), fetchNetworkAdapter())
+      mockPool.intercept({ path: '/baz/1337', method: 'GET' }).reply(200, 'res1')
+      await expect(c.get('1337').expect(200)).to.eventually.equal('res1')
+    })
+
+    it('can send a request with headers', async function () {
+      const c = client(new URL('https://test.local/baz/'), fetchNetworkAdapter())
+      mockPool.intercept({
+        path: '/baz/1338',
+        method: 'GET',
+        headers: (headers) => new Headers(headers).get('x-test') === 'test-get-with-headers'
+      }).reply(200, 'res2')
+      await expect(c.get('1338', {
+        'X-Test': 'test-get-with-headers'
+      }).expect(200)).to.eventually.equal('res2')
+    })
+  })
+
+  describe('#post()', function () {
+    it('can send a request', async function () {
+      const c = client(new URL('https://test.local/baz/'), fetchNetworkAdapter())
+      mockPool.intercept({ path: '/baz/1337', method: 'POST' }).reply(200, 'res1')
+      await expect(c.post('1337').expect(200)).to.eventually.equal('res1')
+    })
+
+    it('can send a request with data', async function () {
+      const c = client(new URL('https://test.local/baz/'), fetchNetworkAdapter())
+      mockPool.intercept({
+        path: '/baz/1338',
+        method: 'POST',
+        body: '{"foo":"bar"}'
+      }).reply(200, 'res2')
+      await expect(c.post('1338', { foo: 'bar' }).expect(200)).to.eventually.equal('res2')
+    })
+
+    it('can send a request with data and headers', async function () {
+      const c = client(new URL('https://test.local/baz/'), fetchNetworkAdapter())
+      mockPool.intercept({
+        path: '/baz/1339',
+        method: 'POST',
+        body: '{"foo":"bar"}',
+        headers: (headers) => new Headers(headers).get('x-test') === 'test-post-with-headers'
+      }).reply(200, 'res3')
+      await expect(c.post('1339', { foo: 'bar' }, {
+        'X-Test': 'test-post-with-headers'
+      }).expect(200)).to.eventually.equal('res3')
+    })
+  })
+
+  describe('#put()', function () {
+    it('can send a request', async function () {
+      const c = client(new URL('https://test.local/baz/'), fetchNetworkAdapter())
+      mockPool.intercept({ path: '/baz/1337', method: 'PUT' }).reply(200, 'res1')
+      await expect(c.put('1337').expect(200)).to.eventually.equal('res1')
+    })
+
+    it('can send a request with data', async function () {
+      const c = client(new URL('https://test.local/baz/'), fetchNetworkAdapter())
+      mockPool.intercept({
+        path: '/baz/1338',
+        method: 'PUT',
+        body: '{"foo":"bar"}'
+      }).reply(200, 'res2')
+      await expect(c.put('1338', { foo: 'bar' }).expect(200)).to.eventually.equal('res2')
+    })
+
+    it('can send a request with data and headers', async function () {
+      const c = client(new URL('https://test.local/baz/'), fetchNetworkAdapter())
+      mockPool.intercept({
+        path: '/baz/1339',
+        method: 'PUT',
+        body: '{"foo":"bar"}',
+        headers: (headers) => new Headers(headers).get('x-test') === 'test-put-with-headers'
+      }).reply(200, 'res3')
+      await expect(c.put('1339', { foo: 'bar' }, {
+        'X-Test': 'test-put-with-headers'
+      }).expect(200)).to.eventually.equal('res3')
+    })
+  })
+
+  describe('#patch()', function () {
+    it('can send a request', async function () {
+      const c = client(new URL('https://test.local/baz/'), fetchNetworkAdapter())
+      mockPool.intercept({ path: '/baz/1337', method: 'PATCH' }).reply(200, 'res1')
+      await expect(c.patch('1337').expect(200)).to.eventually.equal('res1')
+    })
+
+    it('can send a request with data', async function () {
+      const c = client(new URL('https://test.local/baz/'), fetchNetworkAdapter())
+      mockPool.intercept({
+        path: '/baz/1338',
+        method: 'PATCH',
+        body: '{"foo":"bar"}'
+      }).reply(200, 'res2')
+      await expect(c.patch('1338', { foo: 'bar' }).expect(200)).to.eventually.equal('res2')
+    })
+
+    it('can send a request with data and headers', async function () {
+      const c = client(new URL('https://test.local/baz/'), fetchNetworkAdapter())
+      mockPool.intercept({
+        path: '/baz/1339',
+        method: 'PATCH',
+        body: '{"foo":"bar"}',
+        headers: (headers) => new Headers(headers).get('x-test') === 'test-patch-with-headers'
+      }).reply(200, 'res3')
+      await expect(c.patch('1339', { foo: 'bar' }, {
+        'X-Test': 'test-patch-with-headers'
+      }).expect(200)).to.eventually.equal('res3')
+    })
+  })
+
+  describe('#delete()', function () {
+    it('can send a request', async function () {
+      const c = client(new URL('https://test.local/baz/'), fetchNetworkAdapter())
+      mockPool.intercept({ path: '/baz/1337', method: 'DELETE' }).reply(200, 'res1')
+      await expect(c.delete('1337').expect(200)).to.eventually.equal('res1')
+    })
+
+    it('can send a request with headers', async function () {
+      const c = client(new URL('https://test.local/baz/'), fetchNetworkAdapter())
+      mockPool.intercept({
+        path: '/baz/1338',
+        method: 'DELETE',
+        headers: (headers) => new Headers(headers).get('x-test') === 'test-delete-with-headers'
+      }).reply(200, 'res2')
+      await expect(c.delete('1338', {
+        'X-Test': 'test-delete-with-headers'
+      }).expect(200)).to.eventually.equal('res2')
+    })
+  })
+
   describe('#use()', function () {
     it('passes the request through the middleware stack', async function () {
       const originalRequest: Request = {
