e2e452ed1299108e9ec4a8ac67a448e800c6104e
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2023-03-25 21:29:58 +0100

    feat: Parse "Expires" cookie attribute
    This adds a dependency on "set-cookie-parser", which does the heavy
    lifting of cookie parsing. I thought about implementing this myself, but
    it's simply not worth it. For now, our memory cookie store makes use of
    the "expires" attribute so that cookies have a maximum lifetime, and can
    also be deleted by the server by setting "expires" to a past date.

--- test/stores/memory-cookie-store.test.ts
+++ test/stores/memory-cookie-store.test.ts
@@ -1,6 +1,7 @@
 import assert from 'node:assert'
 import { memoryCookieStore } from '../../src/stores/memory-cookie-store.js'
 import { Cookie } from '../../src/index.js'
+import { setTimeout } from 'node:timers/promises'
 
 describe('stores/memory-cookie-store.ts', function () {
   it('initially returns empty cookies array', function () {
@@ -38,5 +39,25 @@ describe('stores/memory-cookie-store.ts', function () {
     }
     store.putCookie(cookie2)
     assert.deepStrictEqual(store.cookies, [cookie2])
+  })
+
+  it('removes cookies when expires is in the past', function () {
+    const store = memoryCookieStore()
+    store.putCookie({ key: 'foo', value: 'bar' })
+    store.putCookie({ key: 'foo', value: 'baz', expires: new Date(0) })
+    assert.deepStrictEqual(store.cookies, [])
+  })
+
+  it('does not return cookies that have expired', async function () {
+    const store = memoryCookieStore()
+    const cookie = {
+      key: 'foo',
+      value: 'bar',
+      expires: new Date(Date.now() + 50)
+    }
+    store.putCookie(cookie)
+    assert.deepStrictEqual(store.cookies, [cookie])
+    await setTimeout(100)
+    assert.deepStrictEqual(store.cookies, [])
   })
 })
